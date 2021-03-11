515469cf4d28d1d722ac5e9bc167495ad88c8362
trueknow <rami.ferjani@esprit.tn>
2021-03-11 22:33:40 +0100

    First Commit

---
+++ src/main/java/tn/esprit/spring/Pidevg2Application.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pidevg2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pidevg2Application.class, args);
+	}
+
+}
