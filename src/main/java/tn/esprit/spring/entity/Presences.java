814ab2e1b76588074ab453e4d565ba979512a2ad
sameh <sameh.khelifi@esprit.tn>
2021-03-22 21:34:34 +0100

    i finished my tasks just if there is any problem notify me!!

---
+++ src/main/java/tn/esprit/spring/entity/Presences.java
@@ -0,0 +1,29 @@
+package tn.esprit.spring.entity;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+
+
+public class Presences {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private Integer user_id;
+	private String date;
+	private Boolean present;
+	private String obs;
+	private String activity;
+	private String professor;
+	private String discipline;
+
+}
---
+++ src/main/java/tn/esprit/spring/service/PresencesListWrapper.java
@@ -0,0 +1,16 @@
+package tn.esprit.spring.service;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+import tn.esprit.spring.entity.Presences;
+
+public class PresencesListWrapper {
+	
+	private List<Presences> presencesList;
+
+}
