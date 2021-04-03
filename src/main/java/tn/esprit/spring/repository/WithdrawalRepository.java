6aebbe22c81cc9fd0a84925cabc9b9e7dcbf1265
Sarratarchoul <58078473+Sarratarchoul@users.noreply.github.com>
2021-04-03 16:16:09 +0100

    Delete WithdrawalRepository.java

--- src/main/java/tn/esprit/spring/repository/WithdrawalRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.spring.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import tn.esprit.spring.entity.Withdrawal;
-
-public interface WithdrawalRepository extends CrudRepository<Withdrawal, Long> {
-
-}
