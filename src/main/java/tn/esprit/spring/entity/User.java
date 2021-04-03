1fd30282f87a6dee48c66105600f7bfb7b858e92
trueknow <49539060+trueknow@users.noreply.github.com>
2021-04-03 14:53:35 +0100

    association yasmine sarra

--- src/main/java/tn/esprit/spring/entity/User.java
+++ src/main/java/tn/esprit/spring/entity/User.java
@@ -9,13 +9,22 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 import java.io.Serializable;
+import java.util.Set;
 
+import tn.esprit.spring.entity.*;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column; 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -29,6 +38,8 @@ import lombok.Setter;
 @Table( name = "T_USER")
 public class User  implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
+	
 	
 	@Id @Setter @Getter @GeneratedValue (strategy = GenerationType.IDENTITY)
 	@Column(name="USER_ID")
@@ -38,7 +49,115 @@ public class User  implements Serializable {
 	@NotBlank(message = "E-mail required") @Column(name="MAIL")@Email(message = "E-mail invalid") @Getter @Setter private String email;
 	@Getter @Setter @Column(name="PASSWORD")@NotBlank(message = "Password required") @Min(6) @Max(60) private  String password;
 	@Getter @Setter@Column(name="ACTIVE") private  Boolean isActive = false;
-	@Getter @Setter private  ProfileEnum profile;
+	@Getter @Setter @Enumerated(EnumType.STRING)private  ProfileEnum profile;
+	
+	@OneToMany (cascade = CascadeType.ALL, mappedBy ="user" )
+	private Set<Comment> Comment;
+
+	@OneToMany (cascade = CascadeType.ALL, mappedBy ="user" )
+	private Set<Like> Like;
+	
+	@OneToMany (cascade = CascadeType.ALL, mappedBy ="user" )	
+	private Set<Rating> Rating;
+
+	@OneToMany (cascade = CascadeType.ALL, mappedBy ="user" )
+	private Set<Post> Post;
+
+	@OneToMany (cascade = CascadeType.ALL, mappedBy ="user" )
+	private Set<Topic> Topic;
+	
+	@OneToOne(mappedBy="User")
+	private Account account;
+	
+	public User( @NotBlank(message = "Display name required ") @Size(min = 4) String name,
+			@NotBlank(message = "Display name required") @Size(min = 4) String prename,
+			@NotBlank(message = "E-mail required") @Email(message = "E-mail invalid") String email,
+			@NotBlank(message = "Password required") @Min(6) @Max(60) String password, Boolean isActive,
+			ProfileEnum profile) {
+		super();
+		this.name = name;
+		this.prename = prename;
+		this.email = email;
+		this.password = password;
+		this.isActive = isActive;
+		this.profile = profile;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getPrename() {
+		return prename;
+	}
+
+
+	public void setPrename(String prename) {
+		this.prename = prename;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public Boolean getIsActive() {
+		return isActive;
+	}
+
+
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+
+
+	public ProfileEnum getProfile() {
+		return profile;
+	}
+
+
+	public void setProfile(ProfileEnum profile) {
+		this.profile = profile;
+	}
+
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
 	
 	
 	
