845dc3b1e554a147c7dec20b315eec2da8862ec3
yasmine175 <58077793+yasmine175@users.noreply.github.com>
2021-04-04 15:28:34 +0100

    update yasmine

--- src/main/java/tn/esprit/spring/entity/Comment.java
+++ src/main/java/tn/esprit/spring/entity/Comment.java
@@ -10,6 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+
 @Entity
 @Table(name ="comments")
 public class Comment implements Serializable {
