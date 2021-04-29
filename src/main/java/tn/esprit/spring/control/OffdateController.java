f861c9d72626485df8a68d81b3ce84a98f059255
sameh-yasuo <57291152+sameh-yasuo@users.noreply.github.com>
2021-04-30 00:45:27 +0100

    Update OffdateController.java

--- src/main/java/tn/esprit/spring/control/OffdateController.java
+++ src/main/java/tn/esprit/spring/control/OffdateController.java
@@ -1,118 +1,106 @@
 package tn.esprit.spring.control;
 
-
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import tn.esprit.spring.service.VacationService;
-import tn.esprit.spring.entity.Offdate;
 
-import java.util.List;
-//@CrossOrigin("http://localhost:3306")
+import tn.esprit.spring.entity.Offdate;
+import tn.esprit.spring.service.VacationService;
+import tn.esprit.spring.service.VacationServiceEmpl;
+
 @RestController
 public class OffdateController {
-    @Autowired
-    private VacationService VacationService;
-
-//najmou nekhdmouhom lkol b REQUEST MAPPING?????
+	@Autowired
+	VacationService VacationService;
+	@Autowired
+	VacationServiceEmpl vc ;
+    //admin role
+	@GetMapping("/GETall-Offdates")
+	@ResponseBody
+	public List<Offdate> getOffdates() {
+	List<Offdate> list = VacationService.getAllOffdates();
+	return list;
+	}
+	//admin role
+	@GetMapping("/getOffdate/{congeID}")
+	@ResponseBody
+	 public Offdate getbyidOffdate(@PathVariable("congeID") String coId) {
+	 return VacationService.getbyidOffdate(coId);
+	 }
+    //employee role
+	@PostMapping("/add-Offdate/{id}/{id1}")
+	@ResponseBody
+	public Offdate createOffdate(@RequestBody Offdate v,@PathVariable("id") String UserId,@PathVariable("id1")String User1Id) {
+		Offdate Offd = VacationService.createOffdate(v,UserId,User1Id);
+	 return Offd;
+	 }
+	//admin role
+	@DeleteMapping("/delete-Offdate/{congeID}")
+	@ResponseBody
+	public ResponseEntity<String> removeComment(@PathVariable("congeID") String coID) {
+		VacationService.deleteOffdate(coID);
+		return new ResponseEntity<String>("vacation deleted successfully",HttpStatus.OK);
+	 }
     
-    @GetMapping("/rest/getAllOffdate")
+	@PutMapping("/modify-Offdate")
+	@ResponseBody
+	 public Offdate modifyOffdate (@RequestBody Offdate Offdate) {
+	 return VacationService.updateOffdate(Offdate);
+	 }
+	//Admin Role
+	@GetMapping(value = "findAlloffdatesbyuid/{Sender}")
     @ResponseBody
-    public List<Offdate>getAllUser(){
-        List<Offdate> list = VacationService.getAllOffdates();
-        return list;
-    }
+	public List<Offdate> findAllOffdateByUserJPQL(@PathVariable("Sender") int UserId) {
 
-    
-    @GetMapping("/rest/getbyidOffdate")
-    @ResponseBody
-    public Offdate getOffdate(String congeID){
-    	Offdate Offdate = VacationService.getbyidOffdate( congeID);
-        return Offdate;
-    }
-
-    
-    @PostMapping("/rest/createOffdate")
-    @ResponseBody
-   public String createOffdate(@RequestBody Offdate v){
-       
-    	Offdate Offdate = VacationService.getbyidOffdate(Long.toString(v.getCongeID()));
-        if(Offdate!=null){
-           
-            return "the Vacation has been registered,please register again";
-       }else{
-        	VacationService.createOffdate(v);
-            return "the enployee with the vacation demand id  was  added";
-        }
-    }
-
-    @DeleteMapping("/rest/deleteOffdate")
-    @ResponseBody
-    public String deleteOffdate(String congeID){
-            	Offdate Offdate = VacationService.getbyidOffdate( congeID);
-        if(Offdate != null){
-        	VacationService.deleteOffdate( congeID);
-            return "the VACATION has been deleted";
-        }else{
-            return "no found";
-        }
-
-    }
-    @Override
-	public List<Offdate> findAllOffdateByUserJPQL(int UserId){
-		return VacationRepository.findAllOffdateByUserJPQL(UserId);
-		
+		return VacationService.findAllOffdateByUserJPQL(UserId);
 	}
-	public long calculateDateInterval(Date startDate, Date endDate) {
-		return ChronoUnit.DAYS.between(startDate.toInstant(), endDate.toInstant());
+	//Admin Role
+	@PutMapping("/update-vacAccepted/{congeID}")
+  	@ResponseBody
+  	public ResponseEntity<String> updatevacaccepted(@PathVariable("congeID")Long congeID) {
 		
-	}
-	public void acceptOffdateJPQL(Long congeID) {
-		VacationRepository.acceptOffdateJPQL(congeID);
+		vc.acceptOffdateJPQL(congeID);
+  		return new ResponseEntity<String>("vacation updated successfully",HttpStatus.OK);
+  		
+  	}
+	//Admin role
+	@PutMapping("/update-vacDenied/{congeID}")
+  	@ResponseBody
+  	public ResponseEntity<String> updatevacDenied(@PathVariable("congeID")Long congeID) {
 		
-	}
-	public void DeniedOffdateJPQL(Long congeID) {
-		VacationRepository.DeniedOffdateJPQL(congeID);
+		vc.DeniedOffdateJPQL(congeID);
+  		return new ResponseEntity<String>("vacation updated successfully",HttpStatus.OK);
+  		
+  	}
+	//employee role
+	@PutMapping("/update-vacInprogress/{congeID}")
+  	@ResponseBody
+  	public ResponseEntity<String> updatevacInprogress(@PathVariable("congeID")Long congeID) {
 		
+		vc.InprogressOffdateJPQL(congeID);
+  		return new ResponseEntity<String>("vacation in progress",HttpStatus.OK);
+  		
+  	}
+	@GetMapping(value = "/MyInprogressList")
+	public List<Offdate> MyInprogressList(){
+		List<Offdate> list = vc.MyInprogressList();
+		return list;
 	}
-	public void InprogressOffdateJPQL(Long congeID) {
-		VacationRepository.InprogressOffdateJPQL(congeID);
-		
+	@GetMapping(value = "/acceptableList")
+	public List<Offdate> findAllacceptableOffdate(){
+		List<Offdate> list = vc.findAllacceptableOffdate();
+		return list;
 	}
-	public List<Offdate> MyInprogressList() {
-		List<Offdate> my =VacationRepository.MyInprogressList();
-		return  my;
-}
-	public List<Offdate> findAllacceptableOffdate() {
-		List<Offdate> acceptable =VacationRepository.findAllacceptableOffdate();
-		return  acceptable;
-}
-}
-
-    @PutMapping("/rest/updateOffdate")
-    @ResponseBody
-   public String updateOffdate(@RequestBody Offdate v){
-    	Offdate Offdate = VacationService.getbyidOffdate(Long.toString(v.getCongeID()));
-
-        if(Offdate!=null){
-        	VacationService.updateOffdate(v);
-           return "update successfully";
-        }else{
-            return "update......no found";
-        }
-
-    }
-
-
-
 
 }
