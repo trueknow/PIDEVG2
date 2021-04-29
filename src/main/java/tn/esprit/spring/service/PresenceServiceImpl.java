1be8bbca546de7df28000428f4fdc15e6b995285
sameh-yasuo <57291152+sameh-yasuo@users.noreply.github.com>
2021-04-30 00:50:51 +0100

    Update PresenceServiceImpl.java

--- src/main/java/tn/esprit/spring/service/PresenceServiceImpl.java
+++ src/main/java/tn/esprit/spring/service/PresenceServiceImpl.java
@@ -1,39 +1,82 @@
 package tn.esprit.spring.service;
 
 
-import tn.esprit.spring.entity.Presences;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-@Service()
+
+import tn.esprit.spring.entity.Presences;
+import tn.esprit.spring.entity.User;
+import tn.esprit.spring.repository.PresencesRep;
+import tn.esprit.spring.repository.UserRepository;
+
+@Service
 public class PresenceServiceImpl implements PresenceService{
-    private List<Presences> Pres ;
-   
 
-    @Override
-    public List<Presences> getPresences() {
-        return Pres;
-    }
+@Autowired
+PresencesRep PresencesRep ;
+@Autowired
+UserRepository UserRepository; 
 
-    @Override
-    public void addPresences(Presences Presences) {
-    	Presences.add(Presences) ;
-    }
 
-    @Override
-    public void UpdatePresences(Presences Presences) {
-    	Presences.remove(Presences);
-    	Presences.add(Presences);
-    }
+	private static final Logger L = LogManager.getLogger(VacationServiceEmpl.class);
 
-    @Override
-    public void deletePresences(int P_id) {
-    	Presences Presences=new Presences();
-    
-     Pres.remove(Presences) ;
+	@Override
+	public List<Presences> getAllPresences(){
 
-    }
+		List<Presences> Presences = (List<Presences>) PresencesRep.findAll();
+		for(Presences Presence : Presences)
+			L.info("Presence +++ : " + Presence);
+
+		return Presences;
+
+	}
+
+	
+	@Override
+	public Presences getbyidPresences(String P_id){
+		L.info("in retrievePresences id = " +  P_id);
+		
+		Presences O = PresencesRep.findById(Long.parseLong( P_id)).orElse(null);
+		
+		L.info("Presences returned :" + O);
+		return O ;
+
+	}
+	@Override
+	public Presences createPresences(Presences v, String User){
+
+		Presences	pSaved = PresencesRep.save(v);
+		
+		User user = UserRepository.findById(Long.parseLong( User)).orElse(null);
+		System.out.println(user.getId());
+		
+		v.setUser(user);
+		return pSaved;
+	}
+	@Override
+	public Presences updatePresences(Presences v){
+
+		return PresencesRep.save(v);
+	}
+	
+	@Override
+	public void deletePresences(String P_id){
+		
+		PresencesRep.deleteById(Long.parseLong( P_id));
+	}
+
+	
+	public List<Presences> ListPresentEmpDaily() {
+		List<Presences> work =PresencesRep.ListPresentEmpDaily();
+		return  work;
+}
+	public List<Presences> ListAbsentEmpDaily() {
+		List<Presences> work =PresencesRep.ListAbsentEmpDaily();
+		return  work;
+}
 }
