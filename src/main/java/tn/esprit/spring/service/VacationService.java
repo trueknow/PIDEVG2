48bda14d87ec95b8ab19d23ed85c5f4204b67d3a
sameh-yasuo <57291152+sameh-yasuo@users.noreply.github.com>
2021-04-30 00:48:53 +0100

    Update VacationService.java

--- src/main/java/tn/esprit/spring/service/VacationService.java
+++ src/main/java/tn/esprit/spring/service/VacationService.java
@@ -2,13 +2,17 @@ package tn.esprit.spring.service;
 
 import java.util.List;
 import tn.esprit.spring.entity.Offdate;
+import tn.esprit.spring.entity.User;
 
 public interface VacationService {
 	
 	public List<Offdate> getAllOffdates();
 	public Offdate getbyidOffdate(String congeID);
-	public Offdate createOffdate(Offdate v);
+	public Offdate createOffdate(Offdate v,String UserId,String User1Id);
 	public Offdate updateOffdate(Offdate v );
 	public void deleteOffdate(String congeID);
+	//public boolean verifConditions(String userid,int duration);
+	public List<Offdate> findAllOffdateByUserJPQL(int UserId);
+	
 
 }
