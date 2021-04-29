6b8c20f05f2b6305bfb650d83aae6dd79959c95c
trueknow <49539060+trueknow@users.noreply.github.com>
2021-04-30 00:25:37 +0100

    Update UserRepository.java

--- src/main/java/tn/esprit/spring/repository/UserRepository.java
+++ src/main/java/tn/esprit/spring/repository/UserRepository.java
@@ -1,11 +1,24 @@
 package tn.esprit.spring.repository;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
-import tn.esprit.spring.entity.User;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import tn.esprit.spring.entity.DAOUser;
+import tn.esprit.spring.entity.ProfileEnum;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long>{
+public interface UserRepository extends CrudRepository<DAOUser, Long>{
+	@Modifying@Transactional
+	@Query("update DAOUser u set u.profile = :prof where u.id = :imiu")
+	void setProfileForDAOUser(@Param("prof")ProfileEnum profile, @Param("imiu")Long id);
+	
+	List<DAOUser> findByName(String name);
+	DAOUser findByemail(String email);
 
 }
