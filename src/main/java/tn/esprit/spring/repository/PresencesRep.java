0bc3d6979febe0bf76461693ca947219ccb56f52
sameh-yasuo <57291152+sameh-yasuo@users.noreply.github.com>
2021-04-30 00:52:16 +0100

    Update PresencesRep.java

--- src/main/java/tn/esprit/spring/repository/PresencesRep.java
+++ src/main/java/tn/esprit/spring/repository/PresencesRep.java
@@ -1,10 +1,21 @@
 package tn.esprit.spring.repository;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+
 import tn.esprit.spring.entity.Presences;
 
 @Repository
 
 public interface PresencesRep extends JpaRepository<Presences, Long> {
-
+	
+	//the admin can check who  is present or absent this day and he can do screen shot or save data in file
+	@Query("select  O  from Presences O where O.present='1' ")
+	public List<Presences> ListPresentEmpDaily();
+	@Query("select O  from Presences O where O.present='0' ")
+	public List<Presences> ListAbsentEmpDaily();
+	
 }
