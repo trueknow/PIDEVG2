3488ca9f95ee12c911fdfd58fb1ac598cdaa26b6
Sarratarchoul <58078473+Sarratarchoul@users.noreply.github.com>
2021-04-30 01:29:37 +0100

    Update IAccountService.java

--- src/main/java/tn/esprit/spring/service/IAccountService.java
+++ src/main/java/tn/esprit/spring/service/IAccountService.java
@@ -3,6 +3,7 @@ package tn.esprit.spring.service;
 import java.util.List;
 
 
+
 import tn.esprit.spring.entity.Account;
 public interface IAccountService {
 	List<Account> retrieveAllAccounts();
@@ -10,5 +11,7 @@ public interface IAccountService {
 	void deleteAccount(String accountId);
 	Account updateAccount(Account a);
 	Account retrieveAccount(String accountId);
+	long addOrUpdateAccount(Account a);
 }
+
 
