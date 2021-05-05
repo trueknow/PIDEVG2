086c48d68d7cda58e873fd7186de7d1013eec180
firaslihidheb <58056766+firaslihidheb@users.noreply.github.com>
2021-05-06 00:48:26 +0100

    Update ChatRoomServiceImpl.java

--- src/main/java/tn/esprit/spring/service/ChatRoomServiceImpl.java
+++ src/main/java/tn/esprit/spring/service/ChatRoomServiceImpl.java
@@ -4,13 +4,14 @@ import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import tn.esprit.spring.entity.ChatMessage;
 import tn.esprit.spring.entity.ChatRoom;
 import tn.esprit.spring.repository.ChatRoomRepository;
-
+@Service
 public class ChatRoomServiceImpl implements ChatRoomService{
-
+@Autowired
     ChatRoomRepository cr;
 	
 	private static final Logger L = LogManager.getLogger(ChatRoomServiceImpl.class);
@@ -47,6 +48,12 @@ public class ChatRoomServiceImpl implements ChatRoomService{
 	public void deleteUser(String id) {
 		cr.deleteById(Long.parseLong(id));
 		
+	}
+	@Override
+	public ChatRoom addChatRoom(ChatRoom c) {
+		ChatRoom ChatRoomSaved = null;
+		ChatRoomSaved = cr.save(c);
+		return ChatRoomSaved;
 	}
 
 }
