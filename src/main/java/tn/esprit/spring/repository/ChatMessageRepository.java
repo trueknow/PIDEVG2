38c94b46f5feb83760090d9afa6e8ad27ffec51f
firaslihidheb <58056766+firaslihidheb@users.noreply.github.com>
2021-05-06 00:45:07 +0100

    Update ChatMessageRepository.java

--- src/main/java/tn/esprit/spring/repository/ChatMessageRepository.java
+++ src/main/java/tn/esprit/spring/repository/ChatMessageRepository.java
@@ -1,14 +1,20 @@
 package tn.esprit.spring.repository;
 
 import tn.esprit.spring.entity.ChatMessage;
-import tn.esprit.spring.entity.MessageStatus;
-//import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
+import tn.esprit.spring.entity.MessageStatus;
+
+
+
+
 
 import org.springframework.data.repository.CrudRepository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface ChatMessageRepository extends CrudRepository<ChatMessage, Long> {
 
-  
+
 }
+
